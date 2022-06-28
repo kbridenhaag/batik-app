a9be40e24a07acde0c6bf4c9913078be28dbee91
pandu-supriyono <mail@supriyono.nl>
2022-06-28 20:02:52 +0200

    fix name of govuk text input props interface

--- app/core/components/Govuk/GovukTextInput.tsx
+++ app/core/components/Govuk/GovukTextInput.tsx
@@ -1,12 +1,12 @@
 import classNames from "classnames"
-import { DetailedHTMLProps, DetailsHTMLAttributes, InputHTMLAttributes } from "react"
+import { DetailedHTMLProps, InputHTMLAttributes } from "react"
 
-export interface GovukTextInput
+export interface GovukTextInputProps
   extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   width?: 30 | 20 | 10 | 5 | 4 | 3 | 2
 }
 
-export function GovukTextInput({ className, width, type = "text", ...props }: GovukTextInput) {
+export function GovukTextInput({ className, width, type = "text", ...props }: GovukTextInputProps) {
   const classes = classNames("govuk-input", { [`govuk-input--width-${width}`]: !!width }, className)
   return <input className={classes} type="text" {...props} />
 }
